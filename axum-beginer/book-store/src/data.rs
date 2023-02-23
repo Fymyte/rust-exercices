3b2a3aa47553c227803a7b8fa811077a66192491
Pierrick Guillaume <pierguill@gmail.com>
2023-02-23 19:46:03 +0100

    add: book-store: add put, post and delete book

--- axum-beginer/book-store/src/data.rs
+++ axum-beginer/book-store/src/data.rs
@@ -2,6 +2,7 @@ use std::{collections::HashMap, sync::Mutex, thread};
 
 use crate::response;
 use crate::{book::Book, response::BookStoreResponse};
+use axum::response::Html;
 use axum::{extract, http::StatusCode, Json};
 use once_cell::sync::Lazy;
 
@@ -49,6 +50,16 @@ pub async fn list_books() -> Json<Vec<Book>> {
         .into()
 }
 
+pub async fn put_book(extract::Json(book): extract::Json<Book>) -> BookStoreResponse<String> {
+    tokio::spawn(async move {
+        let mut data = DATA.lock().unwrap();
+        data.insert(book.id as u32, book.clone());
+        BookStoreResponse::Ok((StatusCode::OK, format!("Put book {book}").into()))
+    })
+    .await
+    .unwrap()
+}
+
 pub async fn get_book(extract::Path(id): extract::Path<u32>) -> BookStoreResponse<Book> {
     thread::spawn(move || {
         let data = DATA.lock().unwrap();
@@ -61,5 +72,62 @@ pub async fn get_book(extract::Path(id): extract::Path<u32>) -> BookStoreRespons
         }
     })
     .join()
+    .unwrap()
+}
+
+pub async fn get_book_form(extract::Path(id): extract::Path<u32>) -> Html<String> {
+    tokio::spawn(async move {
+        let data = DATA.lock().unwrap();
+        match data.get(&id) {
+            Some(book) => format!(
+                concat!(
+                    "<form method=\"post\" action=\"/books/{}/form\">\n",
+                    "<input type=\"hidden\" name=\"id\" value=\"{}\">\n",
+                    "<p><input name=\"title\" value=\"{}\"></p>\n",
+                    "<p><input name=\"author\" value=\"{}\"></p>\n",
+                    "<input type=\"submit\" value=\"Save\">\n",
+                    "</form>\n"
+                ),
+                &book.id, &book.id, &book.title, &book.author
+            ),
+            None => format!("<p>Book id {id} not found</p>"),
+        }
+    })
+    .await
+    .unwrap()
+    .into()
+}
+
+pub async fn post_book_form(form: extract::Form<Book>) -> Html<String> {
+    let new_book = form.0;
+    tokio::spawn(async move {
+        let mut data = DATA.lock().unwrap();
+        if data.contains_key(&(new_book.id as u32)) {
+            data.insert(new_book.id as u32, new_book.clone());
+            format!("<p>{new_book}</p>")
+        } else {
+            format!("Book id not found: {}", new_book.id)
+        }
+    })
+    .await
+    .unwrap()
+    .into()
+}
+
+/// Delete a book from database
+/// id: the id of the book to delete
+/// return Ok if book was found, Err otherwise
+pub async fn delete_book(extract::Path(id): extract::Path<u32>) -> BookStoreResponse<Book> {
+    tokio::spawn(async move {
+        let mut data = DATA.lock().unwrap();
+        if data.contains_key(&id) {
+            let book = data.get(&id).unwrap().clone();
+            data.remove(&id);
+            BookStoreResponse::Ok((StatusCode::OK, book.into()))
+        } else {
+            BookStoreResponse::Err((StatusCode::NOT_FOUND, format!("No book found with id {id}")))
+        }
+    })
+    .await
     .unwrap()
 }
--- axum-beginer/book-store/src/main.rs
+++ axum-beginer/book-store/src/main.rs
@@ -5,12 +5,18 @@ mod response;
 use std::net::SocketAddr;
 
 use axum::{self, routing::get, Router, Server};
+use tracing_subscriber::{prelude::__tracing_subscriber_SubscriberExt, util::SubscriberInitExt};
 
 #[tokio::main]
 async fn main() {
+    tracing_subscriber::registry()
+        .with(tracing_subscriber::fmt::layer())
+        .init();
+
     let app = Router::new()
-        .route("/books", get(data::list_books))
-        .route("/books/:id", get(data::get_book));
+        .route("/books", get(data::list_books).put(data::put_book))
+        .route("/books/:id", get(data::get_book).delete(data::delete_book))
+        .route("/books/:id/form", get(data::get_book_form).post(data::post_book_form));
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
     println!("listening on {addr}");
